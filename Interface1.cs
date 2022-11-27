3863c16fa63f1365e57dbb849b3ff980f1a1c25b
tina <tina.gvozdenovic12@gmail.com>
2022-11-27 20:23:58 +0100

    komentar za promene u kodu

---
+++ Interface1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Interface1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
