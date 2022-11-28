0ecff7a27091638e9ecd92b8b0b6d73187d7badb
tina <tina.gvozdenovic12@gmail.com>
2022-11-29 00:21:06 +0100

    dodat je kod

--- Interface1.cs
+++ Interface1.cs
@@ -4,9 +4,48 @@ namespace Interface1
 {
     class Program
     {
+        interface Zaposleni
+        {
+            public abstract void RadSaStudentima();
+            public abstract string Ime();
+            public abstract long Jmbg();
+            public abstract void Podaci(long jmbg, string ime);
+
+        }
+        class Profesor : Zaposleni
+        {
+            public long jmbg;
+            public string ime;
+            public Profesor(long jmbg, string ime)
+            {
+                this.jmbg = jmbg;
+                this.ime = ime;
+            }
+            public void RadSaStudentima()
+            {
+                Console.WriteLine(" Konsultacije su sredom od 10 - 12 h ");
+            }
+            public long Jmbg()
+            {
+                return jmbg;
+            }
+            public string Ime()
+            {
+                return ime;
+            }
+            public void Podaci(long jmbg, string ime)
+            {
+                Console.WriteLine(" Podaci o profesoru");
+                Console.WriteLine("Jmbg" + this.jmbg);
+                Console.WriteLine(" Ime i prezime:" + ime);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Profesor prof = new Profesor(11111111111111, " Pera Peric");
+            prof.Podaci(prof.Jmbg(), prof.Ime());
+            prof.RadSaStudentima();
+            Console.ReadKey();
         }
     }
 }
